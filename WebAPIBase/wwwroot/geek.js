dfde42c9c89aaef79d39f6218c146b78ccc63be9
szabina1013 <szabina.gajda@stud.uni-corvinus.hu>
2023-11-29 16:40:06 +0100

    kész gyak 10

---
+++ WebAPIBase/wwwroot/geek.js
@@ -0,0 +1,16 @@
+﻿indow.onload = function () {
+    letöltésBefejeződött();
+};
+
+var viccek;
+
+fetch('/jokes.json')
+    .then(response => response.json())
+    .then(data => letöltésBefejeződött(data)
+    );
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    viccek = d;
+}
